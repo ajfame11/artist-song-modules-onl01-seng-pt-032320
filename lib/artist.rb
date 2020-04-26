1cfe6fc265c35f446734a95e19f4682c6569f1fd
aurel fontaine <ajfame11@gmail.com>
2020-04-26 23:36:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Artist
-  extend concerns::Memorable
-  extend concerns::Findable
+  extend Concerns::Memorable
+  extend Concerns::Findable
   attr_accessor :name
   attr_reader :songs
 
